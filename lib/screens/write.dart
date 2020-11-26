56b79a0a641a6269de2e46482529cf47042ad7da
Dhruv Dudeja <dhruvdudeja10@gmail.com>
2020-11-26 13:58:14 +0530

    nfc service test phase 1

--- lib/screens/write.dart
+++ lib/screens/write.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:back/services/nfc.dart';
 
 class WritePage extends StatefulWidget {
   static String id = 'write';
@@ -16,58 +17,57 @@ class _WritePageState extends State<WritePage> {
           child: Icon(Icons.nfc),
           onPressed: () {
             showModalBottomSheet(
-
                 context: context,
                 builder: (context) {
-              
                   return Container(
-                    color: Color(0xff757575),
-                    child: Container(
-                    decoration: BoxDecoration(
-                    color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                  )
-                    ),
-                    child:
-                    Column(
-                      children: <Widget>[
-                        Text('  HOLD NEAR THE TAG',
-                        style: TextStyle(
-                          fontSize: 40,
-
-                          color: Colors.black,
-                          fontFamily: 'Source Sans Pro'
-                        ),),
-SizedBox(
-  height: 50,
-),
-                         Image(image: AssetImage('assets/nfcs.jpg'),width: 300,
-
-                         ),
-  SizedBox(
-    height: 50,
-  ),
-                      RaisedButton(
-
-                        shape: RoundedRectangleBorder(
-                          borderRadius:BorderRadius.circular(50),
+                      color: Color(0xff757575),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              topRight: Radius.circular(20),
+                            )),
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              '  HOLD NEAR THE TAG',
+                              style: TextStyle(
+                                  fontSize: 40,
+                                  color: Colors.black,
+                                  fontFamily: 'Source Sans Pro'),
+                            ),
+                            SizedBox(
+                              height: 50,
+                            ),
+                            Image(
+                              image: AssetImage('assets/nfcs.jpg'),
+                              width: 300,
+                            ),
+                            SizedBox(
+                              height: 50,
+                            ),
+                            RaisedButton(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              child: Text(
+                                'SHARE',
+                                style: TextStyle(
+                                  fontFamily: 'Source Sans Pro',
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              color: Colors.teal,
+                              onPressed: () {
+                                NfcHelper().tagRead();
+                              },
+                            ),
+                          ],
                         ),
-                        child: Text('SHARE',
-                        style: TextStyle(
-                          fontFamily: 'Source Sans Pro',
-                          fontWeight: FontWeight.bold,
-                        ),)
-                        , color: Colors.teal,
-                          onPressed: (){},
-                      ),],
-                    ),
 
-
-
-                    // Text('BRING YOUR PHONE CLOSER',style: TextStyle(fontSize: 20,fontFamily: 'Source Sans Pro' ) ,),
-                  ));
+                        // Text('BRING YOUR PHONE CLOSER',style: TextStyle(fontSize: 20,fontFamily: 'Source Sans Pro' ) ,),
+                      ));
                 });
           }),
       body: SafeArea(
@@ -131,6 +131,18 @@ SizedBox(
                     ),
                   ),
                 )),
+            SizedBox(
+              width: 20,
+            ),
+            RaisedButton(
+                color: Colors.teal,
+                child: Text('write'),
+                onPressed: () {
+                  NfcHelper().ndefWrite(
+                      name: 'Dhruv',
+                      phoneno: '9717701020',
+                      email: 'dhruvdudeja10@gmail.com');
+                }),
           ],
         ),
       ),
---
+++ lib/services/nfc.dart
@@ -0,0 +1,47 @@
+import 'package:nfc_manager/nfc_manager.dart';
+import 'dart:typed_data';
+
+class NfcHelper {
+  void tagRead() {
+    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
+      // result.value = tag.data;
+      print(tag.data);
+      NfcManager.instance.stopSession();
+    });
+  }
+
+  void ndefWrite({String name, String phoneno, String email}) {
+    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
+      Ndef ndef = Ndef.from(tag);
+      if (ndef == null || !ndef.isWritable) {
+        print('tag is not writeable');
+        // NfcManager.instance.stopSession(errorMessage: result.value);
+        return;
+      }
+
+      NdefMessage message = NdefMessage([
+        NdefRecord.createMime(
+            'text/vcard',
+            Uint8List.fromList('''BEGIN:VCARD
+      VERSION:3.0
+      FN:$name
+      TEL:$phoneno
+      EMAIL:$email
+      END:VCARD'''
+                .codeUnits)),
+      ]);
+
+      try {
+        await ndef.write(message);
+        print(message);
+        print('written to tag');
+        // result.value = 'Success to "Ndef Write"';
+        NfcManager.instance.stopSession();
+      } catch (e) {
+        // result.value = e;
+        // NfcManager.instance.stopSession(errorMessage: result.value.toString());
+        return;
+      }
+    });
+  }
+}
